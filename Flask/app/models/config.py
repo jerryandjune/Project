8f1d544ca37c3dc7c15b9399d8703a9b816e8054
jerryandjune <47802809+jerryandjune@users.noreply.github.com>
2020-05-07 00:50:27 +0800

    Update config.py

--- Flask/app/models/config.py
+++ Flask/app/models/config.py
@@ -42,11 +42,11 @@ class Config(object):
     # 是否启用mongodb
     MongoDbEnable = True
     MongoDbAuth = True
-    MongoDbUsername = parse.quote_plus('jack')
-    MongoDbPassword = parse.quote_plus('jackPasswd')
-    MongoDbHost = '54.66.165.206'
-    MongoDbPort = '27017'
-    MongoDbName = 'NlpBallisticAnalysis'
+    MongoDbUsername = parse.quote_plus('xxx')
+    MongoDbPassword = parse.quote_plus('xxxx')
+    MongoDbHost = 'xxxxx'
+    MongoDbPort = 'xxxx'
+    MongoDbName = 'xxxxx'
 
     # 新闻摘要长度
     SummaryLength = 5
